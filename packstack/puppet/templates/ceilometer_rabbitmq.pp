520d836c3781b8c459a77a83ea010b503ffee21c
Lukas Bezdicka <lbezdick@redhat.com>
2015-04-23 10:17:44 +0200

    ipv6"

--- packstack/puppet/templates/ceilometer_rabbitmq.pp
+++ packstack/puppet/templates/ceilometer_rabbitmq.pp
@@ -1,6 +1,6 @@
 $kombu_ssl_ca_certs = hiera('CONFIG_SSL_CACERT_FILE')
 $kombu_ssl_keyfile = hiera('CONFIG_CEILOMETER_SSL_KEY', undef)
-$kombu_ssl_certfile = hiera('CONFIG_CCEILOMETER_SSL_CERT', undef)
+$kombu_ssl_certfile = hiera('CONFIG_CEILOMETER_SSL_CERT', undef)
 
 if $kombu_ssl_keyfile {
   $files_to_set_owner = [ $kombu_ssl_keyfile, $kombu_ssl_certfile ]
